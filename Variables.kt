f2e34e79cf1028b91dfabd1e806a68ed5229a59b
Excy1994 <obregondoribel@gmail.com>
2020-11-26 23:18:01 -0600

    Imprimiendo datos

---
+++ Variables.kt
@@ -0,0 +1,17 @@
+
+fun main()
+{
+    var nombre = "Matilda"
+    nombre = "Excy"
+
+    println("Mi nombre es: $nombre")
+
+    var apellido = "Lopez"
+    apellido = "Barrera"
+
+      println ("Mi apellido es: $apellido")
+
+      
+     var edad = "24"
+     println("mi nombre es $nombre $apellido y tengo $edad años")
+}
